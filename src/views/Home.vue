43f5e6350e061bb7a9bf70565e0c69a0515e62f1
arsal <arsal.hameed@gmail.com>
2020-02-04 06:51:54 +0500

    detail and search page of views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="forecast-table">
+      <div class="container">
+
+        <weather-list></weather-list>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WeatherList from "../components/WeatherList";
+import {mapActions, mapMutations, mapState} from "vuex";
 
 export default {
   name: 'home',
+  data() {
+    return {
+      loadingMessage: null,
+      defaults: [
+        { title: "Istanbul" },
+        { title: "Berlin" },
+        { title: "London" },
+        { title: "Helsinki" },
+        { title: "Dublin" },
+        { title: "Vancouver" }
+      ]
+    };
+  },
+
   components: {
-    HelloWorld
+    WeatherList
+  },
+  computed: {
+    ...mapState('weather', ['locations', 'errors'])
+  },
+  methods: {
+    ...mapActions({
+      "fetchWeatherDefaults" : "weather/fetchWeatherDefaults",
+      "fetchWeatherDetail" : "weather/fetchWeatherDetail",
+    }),
+    ...mapMutations({
+      'setLocations': 'weather/setLocations'
+    })
+  },
+  async mounted() {
+    const self = this;
+    await this.setLocations(this.defaults)
+
+    for (let i=0; i < self.defaults.length; i++) {
+      await this.fetchWeatherDefaults(self.defaults[i].title);
+    }
+
+    for (let i=0; i < self.defaults.length; i++) {
+      await this.fetchWeatherDetail(this.locations[i].woeid);
+    }
   }
 }
 </script>
---
+++ src/views/search.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="search-result text-center">
+    <div v-if="loading">Searching for location {{input}}, please wait...</div>
+    <div v-else>
+      <div v-if="locations.length > 0">
+        <h3>Search result of {{input}}.</h3>
+        <weather-item v-for="(item, itemIndex) in locations" :key="itemIndex"
+                      :item="item"
+        ></weather-item>
+
+      </div>
+      <div v-else>No results were found. Try changing the keyword!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {mapActions, mapState} from 'vuex'
+ import WeatherItem from "../components/WeatherItem";
+  export default {
+    data () {
+      return {
+        loading: true
+      }
+    },
+    components: {
+      WeatherItem
+    },
+    computed: {
+      ...mapState('weather', ['locations', 'error']),
+      input () {
+        return this.$route.params.input
+      }
+    },
+    watch: {
+      $route() {
+        this.searchLocation()
+      }
+    },
+    methods: {
+      ...mapActions('weather', ['lookup', 'fetchWeatherDetail']),
+      async getDetails() {
+        if (!this.locations) return
+
+        for (let i=0; i < this.locations.length; i++) {
+          await this.fetchWeatherDetail(this.locations[i].woeid);
+        }
+      },
+      async searchLocation () {
+        this.loading = true
+        try {
+          await this.lookup(this.input)
+          this.loading = false
+          this.getDetails()
+        } catch(err) {
+          console.log(err)
+        }
+      }
+    },
+    mounted () {
+     if (this.input) {
+        this.searchLocation()
+      }
+    }
+  }
+</script>
