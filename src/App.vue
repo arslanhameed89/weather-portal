8fddf6ddc2760ce9da18c258231b365f3acc0580
arsal <arsal.hameed@gmail.com>
2020-02-05 00:11:52 +0500

    app vue file changed

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,21 @@
     <div class="site-content">
       <div class="site-header">
         <div class="container">
-          <a href="index.html" class="branding">
+          <a href="/" class="branding">
             <img src="images/logo.png" alt="" class="logo">
             <div class="logo-type">
-              <h1 class="site-title">Company name</h1>
-              <small class="site-description">tagline goes here</small>
+              <h1 class="site-title">Weather App</h1>
+              <small class="site-description">Mauqah Vue Js Test</small>
             </div>
           </a>
-
-          <!-- Default snippet for navigation -->
-          <div class="main-navigation">
-            <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-            <ul class="menu">
-              <li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
-              <li class="menu-item"><a href="news.html">News</a></li>
-              <li class="menu-item"><a href="live-cameras.html">Live cameras</a></li>
-              <li class="menu-item"><a href="photos.html">Photos</a></li>
-              <li class="menu-item"><a href="contact.html">Contact</a></li>
-            </ul> <!-- .menu -->
-          </div> <!-- .main-navigation -->
-
-          <div class="mobile-navigation"></div>
 
         </div>
       </div> <!-- .site-header -->
 
-      <div class="hero" data-bg-image="images/banner.png">
+      <div class="hero" data-bg-image="./assets/images/banner.png">
         <div class="container">
           <form  @submit.prevent="search" class="find-location">
-            <input type="text" v-model="input" placeholder="Find your location...">
+            <input type="text" class="search-input" v-model="input" placeholder="Find your location...">
             <input type="submit" value="Find">
           </form>
 
