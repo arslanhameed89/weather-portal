e82ce51ce8cba372f4bb48d58c5648918915e06c
arsal <arsal.hameed@gmail.com>
2020-02-04 06:55:17 +0500

    new routes added to file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import search from "../views/search";
+import details from "../views/details";
 
 Vue.use(VueRouter)
 
@@ -9,7 +11,18 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/search/:input',
+    name: 'search',
+    component: search
+  },
+  {
+    path: '/weather-detail/:woeid',
+    name: 'weather-detail',
+    component: details
   }
+
 ]
 
 const router = new VueRouter({
