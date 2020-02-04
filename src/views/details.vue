084af0b33ab7af7483b066c8485b74578d9610bb
arsal <arsal.hameed@gmail.com>
2020-02-05 00:13:15 +0500

    detail page completed

--- src/views/details.vue
+++ src/views/details.vue
@@ -1,12 +1,53 @@
 <template>
-  <div></div>
+    <div class="forecast-container" v-if="weatherDetailedData">
+        <h1 class="text-center">{{weatherDetailedData.title}} Weather</h1>
+        <weather v-for="(consolidate, consolidateIndex) in consolidated" :key="consolidateIndex"
+                 :title="title(consolidate.applicable_date)"
+                 :icon="consolidate.weather_state_abbr"
+                 :min-temprature="consolidate.min_temp"
+                 :max-temprature="consolidate.max_temp"
+                 :temprature="consolidate.the_temp"
+        />
+    </div>
 </template>
-
 <script>
-  export default {
-    data () {
-      return {
-      }
-    }
-  }
+    import {mapActions} from "vuex";
+    import Weather from "../components/Weather";
+
+    export default {
+        data() {
+            return {
+                weatherDetailedData: null,
+                weekDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            }
+        },
+        components: {
+            Weather
+        },
+        computed: {
+            consolidated() {
+                if (!this.weatherDetailedData) {
+                    return;
+                }
+                return this.weatherDetailedData.consolidated_weather
+            }
+        },
+        methods: {
+            ...mapActions("weather", ["getByWoeid"]),
+            title(date) {
+                const day = new Date(date).getDay()
+                return this.weekDays[day]
+            },
+            async fetchDetail() {
+                const woeid = this.$route.params.woeid;
+                this.weatherDetailedData = await this.getByWoeid(woeid)
+            }
+        },
+        mounted() {
+            this.fetchDetail()
+        }
+    };
 </script>
+
+<style scoped>
+</style>
